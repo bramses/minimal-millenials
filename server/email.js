b3258ba7c799145ba139577612ad7169d3527929
Bram Adams <badams5@u.rochester.edu>
2018-08-05 13:16:19 -0400

    fixed email

--- server/email.js
+++ server/email.js
@@ -1,55 +1,6 @@
 'use strict';
 const nodemailer = require('nodemailer');
 
-// // Generate test SMTP service account from ethereal.email
-// // Only needed if you don't have a real mail account for testing
-// nodemailer.createTestAccount((err, account) => {
-//     // create reusable transporter object using the default SMTP transport
-//     let transporter = nodemailer.createTransport({
-//         host: 'smtp.ethereal.email',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: account.user, // generated ethereal user
-//             pass: account.pass // generated ethereal password
-//         }
-//     });
-
-//     // setup email data with unicode symbols
-//     let recieverAddress = 'susan@gmail.com'
-//     let firstName = 'Susan'
-//     let source = '"Bram" <' + process.env.EMAIL_FROM + '>'
-//     let mailOptions = {
-//         from: source,
-//         to: recieverAddress, // list of receivers
-//         subject: 'Thanks for Pledging!', // Subject line
-//         attachments: [ {
-//             filename: 'minimalism_tips.pdf',
-//             path: './minimalism_tips.pdf'
-//         }],
-//         html: `
-//         <h2>Thank You for Pledging, ${firstName}!</h2>
-//         <p>Attached is a PDF that includes seven quick tips to help you get started with minimalism. Good luck!</p>
-//         <p>Best,</p>
-//         <p>Bram</p>
-//         ` // html body
-//     };
-
-//     // send mail with defined transport object
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             return console.log(error);
-//         }
-//         console.log('Message sent: %s', info.messageId);
-//         // Preview only available when sending through an Ethereal account
-//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-//         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-//         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-//     });
-// });
-
-
 
 // setup email data with unicode symbols
 exports.mailTo = (reciever, firstName) => {
